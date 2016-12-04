7019c525769381d1a733e403a7d297ee32c67219
Olsm <olavolsm@gmail.com>
2016-12-04 09:00:14 -0800

    add some constraints

--- IMDB Favorites/SecondViewController.swift
+++ IMDB Favorites/SecondViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet weak var movieSearch: UISearchBar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
